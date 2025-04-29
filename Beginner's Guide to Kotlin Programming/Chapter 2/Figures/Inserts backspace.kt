929ae2c5d29f71725fd5d2268df722ae64fb5c66
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-04-30 07:07:30 +0800

    Add files via upload

---
+++ Beginner's Guide to Kotlin Programming/Chapter 2/Figures/Inserts backspace.kt
@@ -0,0 +1,10 @@
+fun main(){
+
+    println("\bHello\bKotlin\bWorld\b!")
+    // one every word
+    //result =HellKotliWorl!
+
+    println("\b\bHello\b\bKotlin\b\bWorld\b\b!")
+    // two every word
+    //result =HelKotlWor!
+}
