51e04fe198e5f889809d0c8361e0c277b24a3025
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-05-01 07:39:02 +0800

    Add files via upload

---
+++ Beginner's Guide to Kotlin Programming/Chapter 2/Figures/Inserts carriage return.kt
@@ -0,0 +1,18 @@
+fun main(){
+
+    println("I'm missing please find me\r! I'm here")
+    // one carriage return
+    //result =! I'm here
+
+    println("I'm \rmissing \rplease \rfind \rme\r! I'm here")
+    // five carriage return
+    // result =! I'm here
+
+    println("\rI'm \rmissing please \rfind me\r! I'm here")
+    // random carriage return
+    // result =! I'm here
+
+    println("\rI'm missing please find me! I'm here\r")
+    // first and last
+    //result =I'm missing please find me! I'm here
+}
