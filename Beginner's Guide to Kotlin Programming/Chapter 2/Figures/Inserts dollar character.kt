31a138b766176ae8edda3b5974955b0f321467d5
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-05-01 07:40:18 +0800

    Add files via upload

---
+++ Beginner's Guide to Kotlin Programming/Chapter 2/Figures/Inserts dollar character.kt
@@ -0,0 +1,10 @@
+fun main(){
+    println("Dollar Bill\t\t\tFace On the Bill\t\t\tNotes")
+    println("\$1\t\t\t\t\tGeorge Washington\t\tMost common bill")
+    println("\$2\t\t\t\t\tThomas Jefferson\t\tRare, still legal")
+    println("\$5\t\t\t\t\tAbraham Lincoln\t\t\tUsed often")
+    println("\$10\t\t\t\t\tAlexander Hamilton\t\tTreasury founder")
+    println("\$20\t\t\t\t\tAndrew Jackson\t\t\tVery Common")
+    println("\$50\t\t\t\t\tUlysses S. Grant\t\tLess common")
+    println("\$100\t\t\t\tBenjamin Franklin\t\tHighest common bill")
+}
