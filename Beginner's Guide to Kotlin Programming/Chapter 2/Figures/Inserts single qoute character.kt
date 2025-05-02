5180f913b5e02e0404f856220fbcca9a0071cb57
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-05-02 10:22:25 +0800

    Add files via upload

---
+++ Beginner's Guide to Kotlin Programming/Chapter 2/Figures/Inserts single qoute character.kt
@@ -0,0 +1,13 @@
+fun main(){
+
+    println("can you put me in \'prison\'? Thank you!")
+    // two \'
+    //result =can you put me in 'prison'? Thank you!
+
+    println("can you put me in prison? Not \'me\' idiot!")
+    // two \'
+    //result =can you put me in prison? Not 'me' idiot!
+
+    println("can you put me in prison? Why do I only have one\'!")
+    //result =can you put me in prison? Why do I only have one\'
+}
