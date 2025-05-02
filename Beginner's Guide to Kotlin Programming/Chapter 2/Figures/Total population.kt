839e4817aea47c7b3c237f18198c5684ee0e0069
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-05-02 10:26:51 +0800

    Add files via upload

---
+++ Beginner's Guide to Kotlin Programming/Chapter 2/Figures/Total population.kt
@@ -0,0 +1,10 @@
+fun main(){
+    val city = "Butuan City"
+    val numberOfPeopleInCity = "1 million"
+    val year = "2025"
+
+    println("The total population of " + city +
+            " was " + numberOfPeopleInCity +
+            " in " + year)
+
+}
