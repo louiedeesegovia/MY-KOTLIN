06190e83f95186d4ed08e5ef5afa19a7fc6fefaa
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-05-05 11:19:08 +0800

    Add files via upload

---
+++ Beginner's Guide to Kotlin Programming/Chapter 3/Figures/03. Less than.kt
@@ -0,0 +1,19 @@
+fun main() {
+    print("Enter number 1: ")
+    val num1 = readLine()?.toIntOrNull()
+
+    print("Enter number 2: ")
+    val num2 = readLine()?.toIntOrNull()
+
+    if (num1 != null && num2 != null) {
+        if (num1 < num2) {
+            println("Number 1 ($num1) is less than Number 2 ($num2).")
+            println("Correct!")
+        } else {
+            println("Number 1 ($num1) is greater than or equal to Number 2 ($num2).")
+            println("Sorry try again!")
+        }
+    } else {
+        println("Invalid input. Please enter valid numbers.")
+    }
+}
