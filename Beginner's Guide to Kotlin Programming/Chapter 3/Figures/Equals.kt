f62bdcf03b33936a6e749bca219862d56285fa9d
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-05-05 11:05:04 +0800

    Add files via upload

---
+++ Beginner's Guide to Kotlin Programming/Chapter 3/Figures/Equals.kt
@@ -0,0 +1,13 @@
+fun main() {
+        print("Enter number 1: ")
+        val num1 = readLine()?.toInt()
+
+        print("Enter number 2: ")
+        val num2 = readLine()?.toInt()
+
+        if (num1 == num2) {
+            println("Number 1 and Number 2 are equal.")
+        } else {
+            println("Number 1 and Number 2 are not equal.")
+        }
+    }
