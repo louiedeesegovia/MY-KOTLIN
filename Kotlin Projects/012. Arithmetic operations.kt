5a30697663e801ee5478a9b815b5412f4e20997a
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-05-14 08:18:37 +0800

    Add files via upload

---
+++ Kotlin Projects/012. Arithmetic operations.kt
@@ -0,0 +1,23 @@
+fun main(){
+    val d1 = 8 + 2  // d1 is 10
+    val d2 = 8 - 2  // d2 is 6
+    val d3 = 8 * 2  // d3 is 16
+    val d4 = 8 / 2  // d4 is 4
+    val d5 = 5 % 2  // d5 is 1
+    val d6 = -5 % 2 // d6 is -1
+    var d7 = 1
+    d7++    // d7 is 2
+    d7--    // d7 os 1
+    val d8 = d7++ // d8 is 1, d7 is
+    val d9 = ++d7 // d9 is 3, d7 is
+
+    println("d1 is $d1")
+    println("d2 is $d2")
+    println("d3 is $d3")
+    println("d4 is $d4")
+    println("d5 is $d5")
+    println("d6 is $d6")
+    println("d7 is $d7")
+    println("d8 is $d8")
+    println("d9 is $d9")
+}
