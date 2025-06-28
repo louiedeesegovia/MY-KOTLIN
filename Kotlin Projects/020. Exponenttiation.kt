0388e9c71823dc10a3b5000e3d34d3940860fa76
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-06-28 17:24:58 +0800

    Add files via upload

---
+++ Kotlin Projects/020. Exponenttiation.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+import kotlin.math.pow
+
+fun main() {
+    val number = 8.0
+    val power = 3.0
+    val result = number.pow(power)
+    // result is 512.0
+
+    println("Result is $result")
+}
