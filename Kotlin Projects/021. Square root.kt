208d9f3f3571098c5ea1e4119852ece056c3f63d
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-07-01 09:26:26 +0800

    Add files via upload

---
+++ Kotlin Projects/021. Square root.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication
+
+import kotlin.math.sqrt
+
+fun main(){
+    val number = 100.0
+    val result = sqrt(number)
+    //result is 10.0
+
+    println("result is $result")
+    
+
+}
