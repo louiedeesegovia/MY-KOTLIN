3710213330457a24cb7de7838079e0375a951084
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-07-03 07:47:07 +0800

    Add files via upload

---
+++ Kotlin Projects/024. Decimal logarithm.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication
+import kotlin.math.log10
+
+fun main(){
+    val number = 1000.0
+    val result = log10(number)
+    //result is 3.0
+
+    println("result is $result")
+}
