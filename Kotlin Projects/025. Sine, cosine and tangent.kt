b502eef2d0d89890f1de8b88fe2bd3bb7552e754
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-07-03 07:47:35 +0800

    Add files via upload

---
+++ Kotlin Projects/025. Sine, cosine and tangent.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+
+import kotlin.math.*
+
+fun main(){
+    val sin90 = sin(Math.PI/2)
+    //sin90 is 1.0
+
+    val cos180 = cos(Math.PI)
+    //cos180 is -1.0
+
+    val tan45 = tan(Math.PI/4)
+    //tan45 is 0.9999999999
+
+    println("sin90 is $sin90")
+    println("cos180 is $cos180")
+    println("tan45 is $tan45")
+}
