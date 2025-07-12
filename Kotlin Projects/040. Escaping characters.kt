65e51706e1b08da4ab4ae637fa20044f9d2f2b76
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-07-12 22:34:07 +0800

    Add files via upload

---
+++ Kotlin Projects/040. Escaping characters.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+fun main(){
+    // \t Insert a tab.
+    // \b Insert a backspace.
+    // \n Insert a newline.
+    // \r Insert a carriage return.
+    // \' or ' Insert a single qoute
+    // \" Insert a double qoute.
+    // \\ Insert a backslash character
+
+    val str = "She said \"Hello!\" to me."
+
+    println("str is $str")
+
+}
