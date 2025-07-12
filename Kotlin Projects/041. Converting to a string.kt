9a687b9ad7265203fae2f6d819a6fd21e011a08c
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-07-12 22:34:24 +0800

    Add files via upload

---
+++ Kotlin Projects/041. Converting to a string.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication
+
+fun main(){
+    val charA = 'A'
+    val stringA = charA.toString()
+    // stringA is "A"
+
+    val str = "character $charA"
+
+    println("stringA is $stringA")
+    println("str is $str")
+}
