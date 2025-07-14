81ffe838bb708f562a56015fdb1059b38da3f5d6
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-07-14 21:19:50 +0800

    Add files via upload

---
+++ Kotlin Projects/044. Escape characters.kt
@@ -0,0 +1,36 @@
+package com.example.myapplication
+
+fun main() {
+    // \' for a single quote.
+    val c1 = '\''
+
+    // \" for a double quote.
+    val c2 = '\"'
+
+    // \\ for a backslash.
+    val c3 = '\\'
+
+    // \b for a backspace.
+    val c4 = '\b'
+
+    // \n for a new line.
+    val c5 = '\n'
+
+    // \r for a carriage return.
+    val c6 = '\r'
+
+    // \t for a horizontal tab.
+    val c7 = '\t'
+
+    // Unicode character using hexadecimal code
+    val c8 = '\u00A9' // Copyright symbol
+
+    println("c1 is '$c1'")
+    println("c2 is '$c2'")
+    println("c3 is '$c3'")
+    println("c4 is Hi'$c4'")
+    print("c5 is '$c5'") // To show effect
+    print("c6 is '$c6'")
+    println("c7 is '$c7'")
+    println("c8 is '$c8'")
+}
