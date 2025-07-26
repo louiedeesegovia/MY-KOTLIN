3ed25958513954d63e87c4cf2a88597773c0199a
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-07-26 12:17:57 +0800

    Add files via upload

---
+++ Kotlin Projects/053. Getting values.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication
+
+fun main(){
+    val name = "Alex"
+    val nameExists = name.length > 0
+    //name exists is true
+
+    val number = 7
+    val isTen = number == 10
+    //isTen is false
+
+    println("nameExists is $nameExists")
+    println("isTen is $isTen")
+}
