2eb9f80cd2a7726845363dd7c8c425dd1e48a649
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-07-27 23:17:52 +0800

    Add files via upload

---
+++ Kotlin Projects/055. Initialization.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication
+
+fun main(){
+    val number: Int = 42
+    val nullInt: Int? = null
+    val pi: Double? = 3.14
+    val charA: Char? = 'A'
+    val hasValue: Boolean? = true
+    val noValue: Boolean? = null
+
+    println("number is $number")
+    println("nullInt is $nullInt")
+    println("pi is $pi")
+    println("charA is $charA")
+    println("hasValue is $hasValue")
+    println("noValue is $noValue")
+}
