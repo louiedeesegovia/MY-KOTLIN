641ee938cc42a3169c8926b8a341d823893599ec
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-08-02 23:00:14 +0800

    Add files via upload

---
+++ Kotlin Projects/079. Sets initialization.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication
+
+fun main(){
+    val intSet = setOf<Int>(1, 2, 3)
+    val strSet = setOf("one", "two", "three")
+
+    println("intSet is $intSet")
+    println("strSet is $strSet")
+}
