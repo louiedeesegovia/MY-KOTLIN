1f84ae584a0862770a969242ff813271bd6a2631
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-08-03 09:45:10 +0800

    Add files via upload

---
+++ Kotlin Projects/085. Valid example.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication
+
+fun main(){
+    val latitude = getLatitude()
+    var location = ""
+    if(latitude == 0){
+        location = "Equator"
+    }
+    else if(latitude == 90){
+        location = "north Pole"
+    }
+    else if(latitude == -90){
+        location == "south Pole"
+    }
+    else{
+        location = "not at the Equator or Pole"
+    }
+
+    println("Location is $location")
+}
+
+fun getLatitude(): Int{
+    return (-90..90).random()
+}
